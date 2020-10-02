778d2a48f52ec79786a29d40de71774c61fb93f6
SaitoSeiji <52223899+SaitoSeiji@users.noreply.github.com>
2020-10-02 19:19:17 +0900

    フレームを固定した

--- myPhysX_0930/TestRender.cpp
+++ myPhysX_0930/TestRender.cpp
@@ -4,15 +4,49 @@
 #include "PxPhysicsAPI.h"
 #include "Camera.h"
 #include "RenderActor.h"
+#include <time.h>
 
 using namespace std;
 using namespace physx;
 
 extern void initPhysics();
-extern void stepPhysics();
+extern void stepPhysics(float dt);
 extern void cleanupPhysics();
+extern void keyPress(unsigned char key, const PxTransform& camera);
 namespace {
+	unsigned int frameRange = 500;
 	Camera* sCamera;
+
+	long long beforest=-1;
+	long long beforent=-1;
+	float CalcDt() {
+		timespec _time;
+		if (timespec_get(&_time, TIME_UTC) == 0) {
+			cerr << "Žž‚ÌŽæ“¾‚ÉŽ¸”s‚µ‚½";
+		}
+		long long nowst = _time.tv_sec;
+		long long nownt = _time.tv_nsec;
+		if (beforent < 0) {
+			beforest = nowst;
+			beforent = nownt;
+			return 0;
+		}
+
+		long long dnt = nownt - beforent;
+		beforent = nownt;
+		long dst = 0;
+		if (nowst > beforest) {
+		 dst = nowst - beforest;
+			//cout << dst << "•bŒo‰ß\n";
+			dnt += dst * 1000000000.0f;
+			beforest = nowst;
+		}
+		/*if (dnt < 0) {
+			cout << "framemiss "<< dst<<" "<<dnt<<"\n";
+			return 0;
+		}*/
+		return dnt/100000000.0f;
+	}
 
 	void motionCallback(int x, int y)
 	{
@@ -24,7 +58,7 @@ namespace {
 			exit(0);
 
 		if (!sCamera->handleKey(key, x, y)) {}
-			//keyPress(key, sCamera->getTransform());
+			keyPress(key, sCamera->getTransform());
 	}
 	void mouseCallback(int button, int state, int x, int y)
 	{
@@ -32,11 +66,16 @@ namespace {
 	}
 	void idleCallback()
 	{
+		stepPhysics(CalcDt());
 		glutPostRedisplay();
 	}
-	void renderCallback() {
-		stepPhysics();
 
+	void timerCallback(int militime) {
+		cout << "timer collback\n";
+		//stepPhysics(CalcDt());
+		glutTimerFunc(1000/60, timerCallback, 1);
+	}
+	void renderCallback() {
 		RenderActor::startRender(sCamera->getEye(), sCamera->getDir());
 
 		PxScene* scene;
@@ -57,19 +96,21 @@ namespace {
 	}
 }
 void renderLoop() {
-	cout << "test render";
+	cout << "test render\n";
 	sCamera = new Camera(PxVec3(0.0f, 50.0f, 50.0f), PxVec3(0.0f, -0.1f, -0.7f));
 
 	RenderActor::setupDefaultWindow("test");
 	RenderActor::setupDefaultRenderState();
 
+
+	glutTimerFunc(1000 / 60, timerCallback, 1);
 	glutIdleFunc(idleCallback);
 	glutDisplayFunc(renderCallback);
 	glutKeyboardFunc(keyboardCallback);
 	glutMouseFunc(mouseCallback);
 	glutMotionFunc(motionCallback);
-	motionCallback(0, 0);
 
+	motionCallback(0, 0);
 	atexit(exitCallback);
 
 	initPhysics();
