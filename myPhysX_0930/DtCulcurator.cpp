23b3f17249b90f094dbba832150a9743f847f198
SaitoSeiji <52223899+SaitoSeiji@users.noreply.github.com>
2020-10-19 18:02:51 +0900

    時間計測処理の修正
    時間計測をchronoで統一した

--- myPhysX_0930/DtCulcurator.cpp
+++ myPhysX_0930/DtCulcurator.cpp
@@ -1,6 +1,6 @@
 #include "DtCulcurator.h"
-#include<chrono>
 using namespace std;
+using namespace chrono;
 
 void DtCulcurator::Start() {
 	Reset();
@@ -15,32 +15,18 @@ float DtCulcurator::CalcDt_sec() {
 		cerr << "‰Šú‰»‚³‚ê‚Ä‚¢‚Ü‚¹‚ñ";
 		return -1;
 	}
-	timespec _time;
-	if (timespec_get(&_time, TIME_UTC) == 0) {
-		cerr << "Žž‚ÌŽæ“¾‚ÉŽ¸”s‚µ‚½";
-	}
-	long long nowst = _time.tv_sec;
-	long long nownt = _time.tv_nsec;
-
-	long long dnt = nownt - beforent;
-	beforent = nownt;
-	long dst = 0;
-	if (nowst > beforest) {
-		dst = nowst - beforest;
-		dnt += dst * 1000000000.0f;
-		beforest = nowst;
-	}
-	return dnt / 1000000000.0f;
+	auto now = system_clock::now();
+	auto dur = now - beforePoint; 
+	beforePoint = now;
+	auto misec = duration_cast<std::chrono::microseconds>(dur).count();
+	float sec = misec / 1000000.0f;
+	return sec;
+	
 }
 
 void DtCulcurator::Reset() {
 
-	timespec _time;
-	if (timespec_get(&_time, TIME_UTC) == 0) {
-		cerr << "Žž‚ÌŽæ“¾‚ÉŽ¸”s‚µ‚½";
-	}
-	long long nowst = _time.tv_sec;
-	long long nownt = _time.tv_nsec;
-	beforest = nowst;
-	beforent = nownt;
+	auto now = chrono::system_clock::now();
+	beforePoint = now;
+	
 }
